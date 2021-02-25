ac7ebfa6256b0ccbaef2f82658bf8a2e3e3dffe7
Matheus Letra <jrtyuw@gmail.com>
2021-02-25 19:49:37 -0300

    Commit 25/02/2020 - 19:49 - Searching Heroes Init

--- src/pages/Heroes/index.js
+++ src/pages/Heroes/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { ImageBackground, FlatList, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ImageBackground, FlatList, View, ActivityIndicator, TextInput } from 'react-native';
+import filter from 'lodash.filter';
 import { styles } from './styles';
 
 import { characterController } from '../../controllers/characterController';
@@ -8,17 +9,82 @@ import { HeroDetails } from '../../components/HeroDetails'
 const image = require('../../images/heroesBackground.jpg');
 
 export default function Heroes() {
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([])
+  const [fullData, setFullData] = useState([]);
+  const [error, setError] = useState(null);
 
-  const [data, setData] = React.useState([])
-
-  React.useEffect(() => {
+  useEffect(() => {
     async function getData() {
-      const controller = new characterController();
-      await controller.getAllCharacters()
-      setData(JSON.parse(controller.response))
+      try {
+        setIsLoading(true);
+        const controller = new characterController();
+        await controller.getAllCharacters()
+        setData(JSON.parse(controller.response))
+        setFullData(JSON.parse(controller.response))
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setError(err);
+      }
     }
     getData()
   }, [])
+
+  if (isLoading) {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" color="#5500dc" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.error}>
+        <Text style={{ fontSize: 18 }}>
+          Error fetching data... Check your network connection!
+        </Text>
+      </View>
+    );
+  }
+
+  function renderHeader() {
+    return (
+      <View style={styles.containerSearchHeader}>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          clearButtonMode="always"
+          value={query}
+          onChangeText={queryText => handleSearch(queryText)}
+          placeholder="Search"
+          style={{ backgroundColor: '#fff', paddingHorizontal: 20 }}
+        />
+      </View>
+    );
+  }
+
+  const handleSearch = text => {
+    const formattedQuery = text.toLowerCase();
+    const filteredData = filter(fullData.results, item => {
+      return contains(item, formattedQuery);
+    });
+    setData(filteredData);
+    setQuery(text);
+  };
+
+  const contains = ({ name }, query) => {
+    if (name.includes(query)) {
+      console.log(name)
+      return true;
+    }
+
+    return false;
+  };
+
+
 
   const renderItem = ({ item }) => (
     <HeroDetails {...item} />
@@ -28,6 +94,7 @@ export default function Heroes() {
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
         <FlatList
+          ListHeaderComponent={renderHeader}
           style={styles.list}
           data={data.results}
           renderItem={renderItem}
--- src/pages/Heroes/styles.js
+++ src/pages/Heroes/styles.js
@@ -1,24 +1,42 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-    },
-    image: {
-      flex: 1,
-      resizeMode: 'cover',
-      justifyContent: 'center',
-    },
-    text: {
-      color: 'grey',
-      fontSize: 30,
-      fontWeight: 'bold',
-    },
-    list :{
-      marginTop: '25%', 
-      height: '20%'
-    }
-  });
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'grey',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  list: {
+    marginTop: '25%',
+    height: '20%'
+  },
+  error: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerSearchHeader:
+  {
+    backgroundColor: '#fff',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 20
+  }
 
-export {styles};
+});
+
+export { styles };
