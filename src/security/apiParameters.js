0ba5013a52057ba4e39005feab4662d4c48b201f
Matheus Letra <60162176+MatheusLetra@users.noreply.github.com>
2021-03-08 19:22:27 -0300

    Update apiParameters.js

--- src/security/apiParameters.js
+++ src/security/apiParameters.js
@@ -1,5 +1,7 @@
 const baseURL = 'http://gateway.marvel.com:80/v1/public'
-const APIKey = '45b969ad7fb3be2b0962ea97e35c9ffa'
-const privateKey = '865b2a2ff59a0e1eb5af0aa3978f7d099607d415'
+
+// insert your credentials here
+const APIKey = ''
+const privateKey = ''
 
 export { baseURL, APIKey, privateKey };
